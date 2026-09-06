ffde26af39b7940d8683b5a0a35e96737b8d82c6
Christopher Man <christophergayiuman@gmail.com>
2026-09-06 13:52:28 +0100

    feat: raylib now drawing grid and tiles

--- src/colors.c
+++
@@ -1,6 +0,0 @@
-#include "colors.h"
-
-struct Color grass_green = {34, 139, 34};
-struct Color sky_blue = {135, 206, 235};
-struct Color dark_purple = {75, 0, 130};
-struct Color grey = {90, 90, 90};
--- src/main.c
+++ src/main.c
@@ -1,115 +1,64 @@
-#include "SDL3/SDL_error.h"
-#include "SDL3/SDL_events.h"
-#include "SDL3/SDL_gamepad.h"
-#include "SDL3/SDL_keyboard.h"
-#include "SDL3/SDL_keycode.h"
-#include "SDL3/SDL_rect.h"
-#include "SDL3/SDL_render.h"
-#include "SDL3/SDL_timer.h"
-#include "SDL3/SDL_video.h"
+#include "raylib.h"
 #include <stdio.h>
 #include <string.h>
-#include <SDL3/SDL.h>
-
-#include "colors.h"
-#include "entity.h"
-#include "sdl.h"
 
 // DEFINITIONS
 #define DEBUG_GRID true 
-#define WINDOW_W 800
-#define WINDOW_H 600 
-#define COLS 20
-#define ROWS 15
+#define WINDOW_W 1280 
+#define WINDOW_H 720 
+#define TILE_SIZE 32
+#define COLS 60
+#define ROWS 34
+
+
+// STRUCTS
+enum TileType{
+    AIR,
+    WALL,
+};
+struct WorldTile{
+    enum TileType tileType;
+};
 
 // HELPER
+void DebugGrid(){
+        // Draw grid
+        for (int r=0; r<ROWS; r++){
+            int y = r * TILE_SIZE;
+            DrawLine(0, y, WINDOW_W, y, WHITE);
+        }
+        for (int c = 0; c < COLS; c++){
+            int x = c * TILE_SIZE;
+            DrawLine(x, 0, x, WINDOW_H, WHITE);
+        }
+}
+
+void DrawWorldTiles(struct WorldTile worldArray[ROWS][COLS]){
+    for(int r=0; r < ROWS; r++){
+        for(int c=0; c < ROWS; c++){
+            if(worldArray[r][c].tileType == WALL){
+                DrawRectangle(c*TILE_SIZE, r*TILE_SIZE, TILE_SIZE, TILE_SIZE, GRAY);
+            }
+        }
+    }
+}
 
 // MAIN
-int main (void) {
+int main(void){
+    struct WorldTile worldArray[ROWS][COLS] = {0};
+    worldArray[0][1].tileType = WALL;
 
-    // Window
-    SDL_Window *window = NULL;
-    SDL_Renderer *renderer = init_sdl(&window, WINDOW_W, WINDOW_H);
-    if (!renderer) return 1;
+    InitWindow(WINDOW_W, WINDOW_H, "Balin");
+    SetTargetFPS(120);
 
-    // WORLD TILES
-    int world[COLS][ROWS] = {0};
-    for (int c=0; c<COLS; c++) {
-        for(int r=0; r< ROWS; r++){
-            // fill the world with air
-            world[c][r] = AIR;
-        }
+    while (!WindowShouldClose()){
+        BeginDrawing();
+        ClearBackground(BLACK);
+
+        DrawWorldTiles(worldArray);
+        DebugGrid();
+
+        EndDrawing();
     }
-    world[2][2] = WALL;
-
-    //      ---- Entities----
-    struct Entity entity_array[32];
-    struct Entity player = {
-        .name = "balin",
-        .pos_x = 1,
-        .pos_y = 1,
-        .stats.strength = 99,
-        .stats.faith = 99,
-        .stats.speech = 99,
-    };
-    entity_array[0] = player;
-
-    //      ----main event loop----
-    int running = 1;
-    SDL_Event event;
-    while (running) {
-        while (SDL_PollEvent(&event)){
-            if(event.type == SDL_EVENT_QUIT){
-                running = 0;
-            }
-            if (event.type == SDL_EVENT_KEY_DOWN){
-                switch (event.key.key){
-                    case SDLK_ESCAPE:
-                        running = 0;
-                        break;
-                    case SDLK_W:
-                        player.pos_y -= 1;
-                        break;
-                    case SDLK_S:
-                        player.pos_y += 1;
-                        break;
-                    case SDLK_A:
-                        player.pos_x -= 1;
-                        break;
-                    case SDLK_D:
-                        player.pos_x += 1;
-                        break;
-                }
-            }
-        }
-
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-        SDL_RenderClear(renderer);
-      
-        draw_grid(renderer, COLS, ROWS);
-
-        // draw world tiles
-        for (int c=0; c<COLS; c++) {
-            for(int r=0; r<ROWS; r++){
-                switch(world[c][r]){
-                    case AIR:
-                        break;
-                    case WALL:
-                        draw_tile(renderer, c, r, grey);
-                }
-            }
-        }
-
-        // size of entity_array
-        for (int i=0; i < (sizeof(entity_array) / sizeof(entity_array[0]) ); i++ ){
-            if(strcmp(entity_array[i].name, "balin")){
-                draw_tile(renderer, player.pos_x, player.pos_y, sky_blue);
-            }
-        }
-        // update render buffer
-        SDL_RenderPresent(renderer);
-
-    }
-    shutdown_sdl(window, renderer);
-    return 0;
+    CloseWindow();
 }
