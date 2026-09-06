d6ab04cf398fd6e0baaca7bb9b1cbf45c9f319a3
Christopher Man <christophergayiuman@gmail.com>
2026-09-06 16:15:22 +0100

    feat: player update

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Balin Game
 
-Welcome to this repo which holds the game code for Balin. It is a video game written in C using SLD2 using as little dependancies as possible. 
+Welcome to this repo which holds the game code for Balin. It is a video game written in C using raylib using as little dependancies as possible. 
 
 As for the game, you are Balin a dward stuck in a cave after trying to find some pi. You need to escape and all you have as a axe on your belt and courage. Escape the dungeon with a good story.
--- src/main.c
+++ src/main.c
@@ -1,25 +1,25 @@
 #include "raylib.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 // DEFINITIONS
-#define DEBUG_GRID true 
-#define WINDOW_W 1280 
-#define WINDOW_H 720 
+#define DEBUG_GRID true
+#define WINDOW_W 1280
+#define WINDOW_H 720
 #define TILE_SIZE 32
 #define COLS 60
 #define ROWS 34
 
 #define MAX_ENTITIES 5
 
-
 // STRUCTS
-enum TileType{
+enum TileType {
     AIR,
     WALL,
 };
 
-typedef struct WorldTile{
+typedef struct WorldTile {
     enum TileType tileType;
 } WorldTile;
 
@@ -28,47 +28,46 @@ typedef struct Position {
     int posY;
 } Position;
 
-typedef struct EntityStats{
+typedef struct EntityStats {
     int health;
     int strength;
     int faith;
     int money;
 } EntityStats;
 
-typedef struct Entity{
+typedef struct Entity {
     char* name;
-    struct Position positon;
+    struct Position position;
     struct EntityStats entityStats;
 } Entity;
 
-
 // HELPER
-void DebugGrid(){
-        // Draw grid
-        for (int r=0; r<ROWS; r++){
-            int y = r * TILE_SIZE;
-            DrawLine(0, y, WINDOW_W, y, WHITE);
-        }
-        for (int c = 0; c < COLS; c++){
-            int x = c * TILE_SIZE;
-            DrawLine(x, 0, x, WINDOW_H, WHITE);
-        }
+void DebugGrid() {
+    // Draw grid
+    for (int r = 0; r < ROWS; r++) {
+        int y = r * TILE_SIZE;
+        DrawLine(0, y, WINDOW_W, y, WHITE);
+    }
+    for (int c = 0; c < COLS; c++) {
+        int x = c * TILE_SIZE;
+        DrawLine(x, 0, x, WINDOW_H, WHITE);
+    }
 }
 
-void DrawWorldTiles(WorldTile worldArray[ROWS][COLS]){
-    for(int r=0; r < ROWS; r++){
-        for(int c=0; c < ROWS; c++){
-            if(worldArray[r][c].tileType == WALL){
-                DrawRectangle(c*TILE_SIZE, r*TILE_SIZE, TILE_SIZE, TILE_SIZE, GRAY);
+void DrawWorldTiles(WorldTile worldArray[ROWS][COLS]) {
+    for (int r = 0; r < ROWS; r++) {
+        for (int c = 0; c < COLS; c++) {
+            if (worldArray[r][c].tileType == WALL) {
+                DrawRectangle(c * TILE_SIZE, r * TILE_SIZE, TILE_SIZE, TILE_SIZE, GRAY);
             }
         }
     }
 }
 
-void InitEntityArray(Entity * entityArray[MAX_ENTITIES], int entityCount){
-    Entity player = {
+int InitEntityArray(Entity *entityArray) {
+    entityArray[0] = (Entity){
         .name = "player",
-        .positon = {
+        .position = {
             .posX = 2,
             .posY = 2,
         },
@@ -79,12 +78,11 @@ void InitEntityArray(Entity * entityArray[MAX_ENTITIES], int entityCount){
             .money = 999
         }
     };
-    entityArray[entityCount++] = &player;
 
-    Entity enemy = {
+    entityArray[1] = (Entity){
         .name = "enemy",
-        .positon = {
-            .posX = 2,
+        .position = {
+            .posX = 5,
             .posY = 2,
         },
         .entityStats = {
@@ -94,53 +92,86 @@ void InitEntityArray(Entity * entityArray[MAX_ENTITIES], int entityCount){
             .money = 999
         }
     };
-    entityArray[entityCount++] = &enemy;
+
+    return 2;
 }
 
-void DrawWorldEntities(Entity * entityArray){
-    for(int i=0; i<MAX_ENTITIES; i++){
-        DrawRectangle(
-            entityArray[i].positon.posX,
-            entityArray[i].positon.posY,
-            TILE_SIZE,
-            TILE_SIZE,
-            PURPLE
-        );
+void DrawWorldEntities(Entity *entityArray, int entityCount) {
+    for (int i = 0; i < entityCount; i++) {
+        if(strcmp(entityArray[i].name, "player")){
+            DrawRectangle(
+                entityArray[i].position.posX * TILE_SIZE,
+                entityArray[i].position.posY * TILE_SIZE,
+                TILE_SIZE,
+                TILE_SIZE,
+                BLUE
+            );
+        } else {
+            DrawRectangle(
+                entityArray[i].position.posX * TILE_SIZE,
+                entityArray[i].position.posY * TILE_SIZE,
+                TILE_SIZE,
+                TILE_SIZE,
+                PURPLE
+            );
+        }
     }
 }
 
 // player movement
-void PlayerMovement(){
-    if(IsKeyPressed(KEY_W)){
+void PlayerMovement(Entity *entityArray, int entityCount) {
+    for (int i = 0; i < entityCount; i++) {
+        if (entityArray[i].name == NULL || strcmp(entityArray[i].name, "player") != 0) {
+            continue;
+        }
+
+        if (IsKeyPressed(KEY_W)) {
+            entityArray[i].position.posY -= 1;
+        }
+        if (IsKeyPressed(KEY_S)) {
+            entityArray[i].position.posY += 1;
+        }
+        if (IsKeyPressed(KEY_A)) {
+            entityArray[i].position.posX -= 1;
+        }
+        if (IsKeyPressed(KEY_D)) {
+            entityArray[i].position.posX += 1;
+        }
     }
 }
 
-
 // MAIN
-int main(void){
+int main(void) {
     // World Layer
     WorldTile worldArray[ROWS][COLS] = {0};
     worldArray[0][1].tileType = WALL;
 
     // Entity Layer
-    Entity * entityArray[MAX_ENTITIES] = {0};
-    int entityCount = 0;
-    InitEntityArray(entityArray, entityCount);
-
+    Entity *entityArray = malloc(MAX_ENTITIES * sizeof(Entity));
+    if (entityArray == NULL) {
+        printf("Failed to allocate entity array\n");
+        return 1;
+    }
+    int entityCount = InitEntityArray(entityArray);
 
     // Game loop
     InitWindow(WINDOW_W, WINDOW_H, "Balin");
     SetTargetFPS(120);
 
-    while (!WindowShouldClose()){
+    while (!WindowShouldClose()) {
+        PlayerMovement(entityArray, entityCount);
+
         BeginDrawing();
         ClearBackground(BLACK);
 
         DrawWorldTiles(worldArray);
-        DrawWorldEntities(entityArray);
+        DrawWorldEntities(entityArray, entityCount);
         DebugGrid();
 
         EndDrawing();
     }
     CloseWindow();
+
+    free(entityArray);
+    return 0;
 }
